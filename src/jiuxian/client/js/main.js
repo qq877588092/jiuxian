0de6b5b0f4395c4318aba3796bf047f12c896d31
niminghui <877588092@qq.com>
2019-12-19 10:00:18 +0800

    111

--- src/jiuxian/client/js/main.js
+++ src/jiuxian/client/js/main.js
@@ -1,22 +1,22 @@
 class MainBox {
-    constructor(data) {
-        this.data = data;
-    }
-    //接口
-    init() {
-        this.renderUI();
-    }
-    //渲染
-    renderUI() {
-        this.createMain();
-        // this.createMenu();
-        // this.createEvent();
-    }
-    //创建main标签
-    createMain() {
-        var html = this.data.map((ele, index) => {
-            var html2 = ele.rightminImg.map((item, index) => {
-                return `
+  constructor(data) {
+    this.data = data;
+  }
+  //接口
+  init() {
+    this.renderUI();
+  }
+  //渲染
+  renderUI() {
+    this.createMain();
+    this.toggleShop();
+    // this.createEvent();
+  }
+  //创建main标签
+  createMain() {
+    var html = this.data.map((ele, index) => {
+      var html2 = ele.rightminImg.map((item, index) => {
+        return `
                 <div class="spiritListPic">
                   <a href="#" target="_blank" title="52°汾酒集团贵宾窖藏（红）1500ml"><img
                       src="${item}" width="160"
@@ -24,97 +24,101 @@ class MainBox {
                   </a>
                 </div>
                 `
-            }).join(",");
-            var html3 = ele.rightName.map((item, index) => {
-                return `
+      }).join(",");
+      var html3 = ele.rightName.map((item, index) => {
+        return `
                 <div class="spiritListTit">
                   <a href="#" target="_blank" title="52°汾酒集团贵宾窖藏（红）1500ml">
                       ${item}
                   </a>
                 </div>
                 `
-            }).join(",");
-            var html4 = ele.price.map((item, index) => {
-                return `
+      }).join(",");
+      var html4 = ele.price.map((item, index) => {
+        return `
                 <div class="spiritListPrice">
                   <strong goodid="93181" class="jxIndex_nowPrice_93181">${item}</strong><span><em
                       clubgoodid="93181"></em><i class="clubIcon"></i></span>
                 </div>
                 `
-            }).join(",");
+      }).join(",");
 
 
-            var item4 = ele.price.map((item, index) => {
-                for (var i = 0; i <= index; i++) {
-                    return `
+      var item4 = ele.price.map((item, index) => {
+        for (var i = 0; i <= index; i++) {
+          return `
                 <li>
                     ${html2.split(",")[index]}
                     ${html3.split(",")[index]}
                     ${html4.split(",")[index]}
                 </li>
                 `
-                }
+        }
 
-            }).join("");
+      }).join("");
 
-            var html5 = ele.bottomNameleft1.map((item, index) => {
-                return `
+      var html5 = ele.bottomNameleft1.map((item, index) => {
+        return `
                 <a href="#" target="_blank"
                 title="新品首发"><span>${item}</span></a>
                 `
-            }).join("");
-            var html6 = ele.bottomNameleft2.map((item, index) => {
-                return `
+      }).join("");
+      var html6 = ele.bottomNameleft2.map((item, index) => {
+        return `
                 <a href="#" target="_blank"
                 title="茅台"><span>${item}</span></a>
                 `
-            }).join("");
+      }).join("");
 
-            var html7 = ele.bottomRight.topTenNav.map((item, index) => {
-                return `
+      var html7 = ele.bottomRight.topTenNav.map((item, index) => {
+        return `
                 <a href="javascript:;" ${index==0?"on":""} on="1">${item}</a>
               <span>|</span>
                 `
-            }).join("");
+      }).join("");
 
 
 
 
 
-            var html8 = ele.bottomRight.bottomNameright2.map((ele, index) => {
-                return ele.map((item, index) => {
-                    return `
+      var html8 = ele.bottomRight.bottomNameright2.map((ele, index) => {
+        return ele.map((item, index) => {
+          return `
                 <div class="topTenTit"><a href="#" target="_blank"
                     title="【买一送一】53°国台·品鉴15  500ml">${item}</a></div>
                 `
-                })
-            }).join(",");
-            console.log(html8.split(","));
+        })
+      }).join(",");
 
-
-            var html9 = ele.bottomRight.bottomNameprice.map((ele, index) => {
-                return ele.map((item, index) => {
-                    return `
+      var html9 = ele.bottomRight.bottomNameprice.map((ele, index) => {
+        return ele.map((item, index) => {
+          return `
                 <div class="topTenPrice"><strong class="jxIndex_nowPrice_28877" goodid="28877">${item}</strong>
                 </div>
-              </div>
                 `
-                })
-            }).join(",");
+        })
+      }).join(",");
 
-            var html10 = ele.bottomRight.bottomImg.map((item, index) => {
-                return `
+
+      var html10 = ele.bottomRight.bottomImg.map((ele, index) => {
+        return ele.map((item, index) => {
+          return `
                   <div class="topTenPic"><a href="#" target="_blank"
                       title="【买一送一】53°国台·品鉴15  500ml"><img
                         src=${item} width="90"
-                        height="90" alt="【买一送一】53°国台·品鉴15  500ml"></a></div>
+                        height="90" alt="【买一送一】53°国台·品鉴15  500ml"></a>
+                  </div>
                 `
-            }).join(",");
+        })
+      }).join(",");
 
-            var item10 = ele.bottomRight.bottomImg.map((item, index) => {
-                for (var i = 0; i <= index; i++) {
-                    return `
-                <ul class="clearfix" style="display: ${index==0?"block":"none"};">
+
+
+
+      var item10 = ele.bottomRight.bottomImg.map((ele, index) => {
+        return ele.map((item, index) => {
+          for (var i = 0; i <= index; i++) {
+            return `       
                 <li product-box="28877"><i class="topTenOne"></i>
                     ${html10.split(",")[index]}
                     <div class="topTenTitBox">
@@ -122,15 +126,25 @@ class MainBox {
                         ${html9.split(",")[index]}
                     </div>
                 </li>
-              </ul>
+             
                 `
-                }
-            }).join("");
-  
-            
+          }
+        }).join("")
+      }).join(",");
 
 
-            return `
+      var item11 = ele.bottomRight.bottomImg.map((ele, index) => {
+        for (var i = 0; i <= index; i++) {
+          return `
+        <ul class="clearfix" style="display:${index==0?"block":"none"}">
+        ${item10.split(",")[index]}
+        </ul>
+        `
+        }
+      }).join("");
+
+
+      return `
             <div class="shoppingBox Lay_5 mt10 whiteWine news">
             <div class="titlebox">
             <div class="comTitle"><i class="comIcon1 newIndexIcon">${ele.topNameI}</i><span>${ele.topNameSpan}</span></div>
@@ -179,7 +193,9 @@ class MainBox {
           </div><!-- 白酒馆本周热销商品Start -->
           <div class="topTenConWrap" name="__home_baipaihang">
             <div class="topTenCon">
-              ${item10}
+                
+                ${item11}
+                
             </div>
           </div><!-- 白酒馆本周热销商品End -->
         </div>
@@ -187,24 +203,33 @@ class MainBox {
       </div>
             `
 
-        }).join("")
-        $(".mainBox").append(html);
-    }
+    }).join("")
+    $(".mainBox").append(html);
+  }
+  //点击切换热销商品
+  toggleShop() {
+    $(".topTenNav a").click(function () {
+  $(".topTenNav a").parent().parent().eq($(this).index()).siblings(".topTenConWrap").find("ul").eq($(this).index()).taggle();
+    
 
-    //内容区小banner
-    smallBanner() {
+    })
+  }
 
-    }
+
+  //内容区小banner
+  // smallBanner() {
+
+  // }
 
 }
 
 $.ajax({
-    type: "get",
-    url: "http://127.0.0.1/code/jiuxian/src/jiuxian/server/main.php",
-    data: "data",
-    dataType: "json",
-    success: function (response) {
-        var p1 = new MainBox(response);
-        p1.init();
-    }
+  type: "get",
+  url: "http://127.0.0.1/code/jiuxian/src/jiuxian/server/main.php",
+  data: "data",
+  dataType: "json",
+  success: function (response) {
+    var p1 = new MainBox(response);
+    p1.init();
+  }
 });
